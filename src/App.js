7db9ca386367ee083bc880b3841a378bed8ae951
Nastia Krat <nastyakpi1995@gmail.com>
2023-06-14 15:29:06 +0300

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import TasksManager from "./pages/TasksManager";
-import {DropDownContext} from "./context/DropDown";
+import { DropDownContext } from "./context/DropDown";
 
 
 function App() {
     return (
         <DropDownContext>
+            <div>helllom my mother</div>
             <TasksManager />
         </DropDownContext>
   );
